04e103edda928a9057bce0e49fd7c58c3b722081
Matias Szeinfeld <mattszeindev@gmail.com>
2023-09-06 19:19:00 +0200

    add tomselect filter pattern for weekdays

--- assets/js/hooks/tomselect_hook.js
+++ assets/js/hooks/tomselect_hook.js
@@ -3,6 +3,8 @@ export const TomselectHook = {
     const selectEl = this.el;
     new TomSelect(selectEl, {
       plugins: ['checkbox_options', 'remove_button'],
+      create: true,
+      createFilter: "(^[+|-])([0-9])*([A-Z]{2})$|(^[A-Z]{2})$",
     });
   }
 }
